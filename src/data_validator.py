6a5b14bd994b3f3ab1b65d897fd8f13211e20c76
Enrique <heyerique@users.noreply.github.com>
2018-04-20 00:56:04 +1200

    remove code smell shotgun surgery

--- src/data_validator.py
+++ src/data_validator.py
@@ -13,151 +13,118 @@ class DataValidator:
             self.check_birthday
         )
 
-    @staticmethod
-    def check_empid(data):
+    def regx_match(self, pattern, data):
+        result = None
+        match_obj = re.match(pattern, str(data), re.I)
+        if match_obj:
+            result = match_obj.group(1)
+        return result
+
+    def regx_match_group(self, pattern, data):
+        result = None
+        match_obj = re.match(pattern, str(data), re.I)
+        if match_obj:
+            result = match_obj.groups()
+        return result
+
+    def check_empid(self, data):
         """
         Check if the input empID is valid.
         :return: Formatted empid if the input one is valid,
         otherwise, return None
         Author: Vaishali Patel
         """
-        # Convert the input data to string
-        empid = str(data)
-
-        # Regular expression checks if there are combination of
-        # [A-Z][0-9]{3} e.r E101
-        # :P<empid> Assign to the group with the keyword 'empid'
         pattern = r"^(?P<empid>[A-Z][0-9]{3})$"
-        match_obj = re.search(pattern, empid, re.I)
-        if match_obj:
-            # Get the matched word
-            empid = match_obj.group("empid")
-            # Convert the first letter to Uppercase
-            # and lowercase for rest of them
+        result = self.regx_match(pattern, data)
+        if result is not None:
+            return result.upper()
 
-            return empid.upper()
-        # Return None if no match found
-        return None
+        return result
 
-    @staticmethod
-    def check_gender(data):
+    def check_gender(self, data):
         """
         Check validation of gender
         :param gender: <String>
         :return: washed data
         :Author: Zhiming Liu
         """
-        gender = str(data)
-        pattern_01 = r"^(?P<gender>F\w*|M\w*)$"
-        pattern_02 = r"^(?P<gender>girl|boy)$"
-        match_01 = re.match(pattern_01, gender, re.I)
-        result = None
-        if match_01:
-            gender = match_01.group("gender").upper()
-            result = gender[0]
-        else:
-            match_02 = re.match(pattern_02, gender, re.I)
-            if match_02:
-                gender = match_02.group("gender").upper()
-                result = "F" if gender == "GIRL" else "M"
-        return result
+        pattern_01 = r"^(?P<gender>F|girl|female|miss|ms)$"
+        pattern_02 = r"^(?P<gender>M|boy|male|mister|mr)$"
 
-    @staticmethod
-    def check_age(data):
+        result = self.regx_match(pattern_01, data)
+        if result is not None:
+            return 'F'
+
+        result = self.regx_match(pattern_02, data)
+        if result is not None:
+            return "M"
+
+    def check_age(self, data):
         """
         Check validation of age
         :param age: <String>
         :return: washed data
         :Author: Zhiming Liu
         """
-        age = str(data)
         pattern = r"^(?P<age>[0-9]{1,2})$"
-        match_obj = re.match(pattern, age)
-        if match_obj:
-            # Convert the match to integer and return
-            return "{0:02}".format(int(match_obj.group("age")))
-        # Return None if no match found
+        result = self.regx_match(pattern, data)
+        if result is not None:
+            return "{0:02}".format(int(result))
         return None
 
-    @staticmethod
-    def check_sales(data):
+    def check_sales(self, data):
         """
         Check if the input sales is valid.
         :return: Formatted sales if the input one is valid,
         otherwise, return None
         :Author: Zhiming Liu
         """
-        # Regular expression checks if there are consecutive 3 numbers
-        # :P<salary> Assign to the group with the keyword 'salary'
-        sales = str(data)
         pattern = r"^(?P<sales>[0-9]{1,3})$"
-        match_obj = re.search(pattern, sales)
-        if match_obj:
-            # Convert the match to integer and return
-            return "{0:03}".format(int(match_obj.group("sales")))
-        # Return None if no match found
+        result = self.regx_match(pattern, data)
+        if result is not None:
+            return "{0:03}".format(int(result))
         return None
 
-    @staticmethod
-    def check_bmi(data):
+    def check_bmi(self, data):
         """
         Check if the input BMI is valid.
         :return: Formatted BMI if the input one is valid,
         otherwise, return None
         :Author: Zhiming Liu
         """
-        # Convert the input data to string
-        bmi = str(data)
-
-        # Regular expression checks if any of the specified keywords exists
-        # :P<bmi> Assign to the group with the keyword 'bmi'
-        pattern = r".*(?P<bmi>normal|overweight|obesity|underweight).*"
-        match_obj = re.search(pattern, bmi, re.I)
-        if match_obj:
-            # Get the matched word
-            bmi = match_obj.group("bmi")
-            # Convert the first letter to uppercase
-            # and lowercase for rest of them
-            bmi = " ".join(text[0].upper() + text[1:] for text in bmi.split())
-            return bmi
-        # Return None if no match found
+        pattern = r"^(?P<bmi>normal|overweight|obesity|underweight)$"
+        result = self.regx_match(pattern, data)
+        if result is not None:
+            return " ".join(text[0].upper() + text[1:]
+                            for text in result.split())
         return None
 
-    @staticmethod
-    def check_salary(data):
+    def check_salary(self, data):
         """
         Check if the input salary is valid.
         :return: Formatted salary if the input one is valid,
         otherwise, return None
         :Author: Zhiming Liu
         """
-        # Regular expression checks if there are consecutive 3 numbers
-        # :P<salary> Assign to the group with the keyword 'salary'
-        salary = str(data)
         pattern = r"^(?P<salary>[0-9]{2,3})$"
-        match_obj = re.search(pattern, salary)
-        if match_obj:
-            # Convert the match to integer and return
-            return "{0:03}".format(int(match_obj.group("salary")))
-        # Return None if no match found
+        result = self.regx_match(pattern, data)
+        if result is not None:
+            return "{0:03}".format(int(result))
         return None
 
-    @staticmethod
-    def check_birthday(data):
+    def check_birthday(self, data):
         """
         Check validation of birthday
         :param birthday: <String>
         :return: washed data
         :Author: Zhiming Liu
         """
-        birthday = str(data)
         pattern = r"^([0-9]{1,2})[-/\.]([0-9]{1,2})[-/\.]([0-9]{2}|[0-9]{4})$"
-        match = re.match(pattern, birthday)
-        if match:
-            date = "-".join(match.groups())
-            struct = strptime(date, "%d-%m-%Y")
+        result = self.regx_match_group(pattern, data)
+        if result is not None:
+            date = strptime("-".join(result), "%d-%m-%Y")
             return "{0:02}-{1:02}-{2:04}"\
-                .format(struct.tm_mday, struct.tm_mon, struct.tm_year)
+                .format(date.tm_mday, date.tm_mon, date.tm_year)
         else:
             return None
 
---
+++ src/test/test_view_console.py
@@ -0,0 +1,5 @@
+import unittest
+import unittest.mock
+from io import StringIO
+from controller import Controller
+
